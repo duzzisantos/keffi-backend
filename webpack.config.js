bf75ca23469d8f36c065fad0bc30cdc1b8f654e0
Duzie <duzzi4x@gmail.com>
2023-12-22 22:32:33 +0100

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,1 @@
-const path = require("path");
 
-module.exports = {
-  mode: "production",
-  entry: "./index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-
-  module: {
-    rules: [{}],
-  },
-};
