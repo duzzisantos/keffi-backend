95505eeee0cf9836f5f1074a12f9f86ce4294ad9
Duzie <duzzi4x@gmail.com>
2023-12-22 22:28:37 +0100

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -9,17 +9,6 @@ module.exports = {
   },
 
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-    ],
+    rules: [{}],
   },
 };
